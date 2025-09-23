c6a7d7fc1cfe49168bee293afe12660583bf1ec1
Evertonalencard <evertonalencard24@gmail.com>
2025-09-23 08:37:00 -0300

    teste git

--- enemy.gd
+++ enemy.gd
@@ -6,7 +6,7 @@ class_name  Enemy
 @export var vertical_spead = 100
 @onready var ray_cast_2d = $RayCast2D as RayCast2D
 
-
+#teste
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
