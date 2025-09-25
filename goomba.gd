9ea4f60613834aa948e9494af1150efde602ecf7
cattergabriel33-dot <cattergabriel33@gmail.com>
2025-09-25 08:38:08 -0300

    0.4.1

--- goomba.gd
+++ goomba.gd
@@ -1,4 +1,6 @@
 extends Enemy
+class_name KOOPA
+const KOOPA_FULL_SHAPE =preload()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +11,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+func die():
+	super.die()
+	set_collision_layer_value(3, false)
+	set_collision_layer_value(1, false)
+	get_tree().create_timer(0.5).timeout.connect(queue_free)
