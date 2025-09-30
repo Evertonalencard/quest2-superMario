ea9c422ffcf231db720a78319c944618a6f1163d
cattergabriel33-dot <cattergabriel33@gmail.com>
2025-09-30 07:43:55 -0300

    0.4.5

--- KOOPA.gd
+++ KOOPA.gd
@@ -8,9 +8,24 @@ const KOOPA_SHELL_POSITION =Vector2(0,5)
 @export var slide_speed = 200
 var in_a_shell = false
 
+
 func _ready():
 	collision_shape_2d = KOOPA_FULL_SHAPE
 	
+func die():
+	if !in_a_shell
+		super.die()
+	
+	collision_shape_2d.set_deferred("shape",KOOPA_FULL_SHAPE)
+	collision_shape_2d.set_deferred("shape",KOOPA_FULL_POSITION)
+	in_a_shell =true
+func on_stomp(player_position: Vector2):
+	set_collision_layer_value(3,false)
+	set_collision_layer_value(1,false)
+	set_collision_layer_value(4,true)
+	var movement_direction = 1 if player_position.x <=global_position.x else -1
+	horizontal_speed = -movement_direction * slide_speed
+
 
 
 func _on_area_entered(area: Area2D) -> void:
